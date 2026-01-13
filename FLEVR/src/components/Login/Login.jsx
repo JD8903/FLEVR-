40d1822f0740fc59861b68415adc6af5025a7518
JD8903 <pjay38027@gmail.com>
2026-01-13 18:16:26 +0530

    done

---
+++ FLEVR/src/components/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from './Form'
+import Grid from './Grid'
+
+const Login = () => {
+  return (
+   <><Form />  <Grid /></>
+    
+
+  )
+}
+
+export default Login
